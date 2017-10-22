f766d99d2eaf14b10b2066d54efacbf416d1ccae
aditi137 <aditidutta137@gmail.com>
2017-10-23 02:59:19 +0800

    exclude numbers and non-eng words; display stemmed and original word

--- data_analysis/stemming.py
+++ data_analysis/stemming.py
@@ -1,5 +1,7 @@
 import pandas
 import nltk
+import re
+
 from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
@@ -11,23 +13,26 @@ stemmer = PorterStemmer()
 tokenizer = RegexpTokenizer('\w+')
 
 stopWords = set(stopwords.words('english'))
-posts = pandas.read_csv('posts.csv', encoding='utf-8')['Text']
+posts = pandas.read_csv('posts.csv')['Text']
+
 wordsBeforeStem = []
-wordsAfterStem = {}
+wordsAfterStem = []
+stemTracker = {}
  
 for post in posts:
     words = tokenizer.tokenize(post)
     for word in words:
-        if word not in stopWords:
+        if word not in stopWords and re.match(r"(?i)\b[a-z_-]+\b", word):
             lowerWord = word.lower()
             wordsBeforeStem.append(lowerWord)
-            wordsAfterStem[stemmer.stem(lowerWord)] = lowerWord
- 
-# identify the top-20 most frequent words from the dataset (excluding the stop words) before and after performing stemming.
+            wordsAfterStem.append(stemmer.stem(lowerWord))
+            stemTracker[stemmer.stem(lowerWord)] = lowerWord
+
+# identify the top-20 most frequent words from the dataset (excluding the stop words) before and after performing stemming
 # for each of the top-20 most frequent stems, list their original words from which the stem is reached
-try:
-    print(Counter(wordsBeforeStem).most_common(20))
-    print(Counter(wordsAfterStem).most_common(20))
-except UnicodeEncodeError:
-    print("Please set console properties to display utf-8. For Windows execute:\n\tchcp 65001\n\tset PYTHONIOENCODING=utf-8\n")
+print(Counter(wordsBeforeStem).most_common(20))
+counterAfterStem = Counter(wordsAfterStem).most_common(20)
+print(counterAfterStem)
+for i in counterAfterStem:
+    print(i[0] + ': ' + stemTracker[i[0]])
 print("Finished")
