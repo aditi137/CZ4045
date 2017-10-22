e3ab143c3b3c754b98e26fe2936ff0b877ea7147
aditi137 <aditidutta137@gmail.com>
2017-10-22 23:32:14 +0800

    updated stemming

--- data_analysis/stemming.py
+++ data_analysis/stemming.py
@@ -1,4 +1,3 @@
-#identify the top-20 most frequent words from the dataset (excluding the stop words) before and after performing stemming.
 import pandas
 import nltk
 from nltk.stem import PorterStemmer
@@ -12,19 +11,23 @@ stemmer = PorterStemmer()
 tokenizer = RegexpTokenizer('\w+')
 
 stopWords = set(stopwords.words('english'))
-posts = pandas.read_csv('posts.csv', encoding='utf-8')['Text'][1:4]
-
+posts = pandas.read_csv('posts.csv', encoding='utf-8')['Text']
 wordsBeforeStem = []
-wordsAfterStem = []
-
+wordsAfterStem = {}
+ 
 for post in posts:
     words = tokenizer.tokenize(post)
     for word in words:
         if word not in stopWords:
             lowerWord = word.lower()
             wordsBeforeStem.append(lowerWord)
-            wordsAfterStem.append(stemmer.stem(lowerWord))
-
-print(Counter(wordsBeforeStem))
-print(Counter(wordsAfterStem))
+            wordsAfterStem[stemmer.stem(lowerWord)] = lowerWord
+ 
+# identify the top-20 most frequent words from the dataset (excluding the stop words) before and after performing stemming.
+# for each of the top-20 most frequent stems, list their original words from which the stem is reached
+try:
+    print(Counter(wordsBeforeStem).most_common(20))
+    print(Counter(wordsAfterStem).most_common(20))
+except UnicodeEncodeError:
+    print("Please set console properties to display utf-8. For Windows execute:\n\tchcp 65001\n\tset PYTHONIOENCODING=utf-8\n")
 print("Finished")
