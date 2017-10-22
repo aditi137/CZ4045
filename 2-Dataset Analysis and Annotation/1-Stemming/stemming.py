4ac1d54d134f7a190af6e00c659d838fc09cefca
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-10-22 23:52:56 +0800

    Reorganised project Added stemming

---
+++ 2-Dataset Analysis and Annotation/1-Stemming/stemming.py
@@ -0,0 +1,51 @@
+import os
+import re
+import pandas
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from collections import Counter
+
+nltk.download('stopwords')
+
+# Functions
+
+def countSortAndSave(list, fileName):
+    count = Counter(list)
+    countDataFrame = pandas.DataFrame.from_dict(count, orient='index').reset_index()
+    countDataFrame.columns = ['stem', 'count']
+    countDataFrame.sort_values(by='count', ascending=False, inplace=True)
+    countDataFrame.to_csv(fileName, index=False, encoding='utf-8')
+
+def main():
+    inputSubdir = 'Input data'
+    outputSubdir = 'Output data'
+    if not os.path.exists(outputSubdir):
+        os.makedirs(outputSubdir)
+
+    stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer('\w+')
+
+    stopWords = set(stopwords.words('english'))
+    posts = pandas.read_csv(os.path.join(inputSubdir, 'posts.csv'), encoding='utf-8')['Text']
+
+    wordsBeforeStem = []
+    wordsAfterStem = []
+
+    for post in posts:
+        words = tokenizer.tokenize(post)
+        for word in words:
+            if word not in stopWords and not bool(re.search(r'\d', word)):
+                lowerWord = word.lower()
+                wordsBeforeStem.append(lowerWord)
+                wordsAfterStem.append(stemmer.stem(lowerWord))
+
+    countSortAndSave(wordsBeforeStem, os.path.join(outputSubdir, 'wordsBeforeStemming.csv'))
+    countSortAndSave(wordsAfterStem, os.path.join(outputSubdir, 'wordsAfterStemming.csv'))
+    print("Finished")
+
+main()
+
+
+
