9db3a947a2ff66faad50e843d3dcbfab5200fb62
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-10-24 00:50:59 +0800

    pos_tagging: Fixed averaged_perceptron_tagger requirement Added tagging for qualitySentences, since the results from a normal random tagging is horrible due to code mixed in the sentences.

--- 2-DatasetAnalysisAndAnnotation/2-POSTagging/pos_tagging.py
+++ 2-DatasetAnalysisAndAnnotation/2-POSTagging/pos_tagging.py
@@ -1,7 +1,19 @@
 import nltk
+import re
 import random
 import pandas
 import os
+
+nltk.download('averaged_perceptron_tagger')
+
+def tagSentencesFrom(sentences, fileName):
+    data = []
+    
+    for sent in random.sample(sentences, 10):
+        data += [nltk.pos_tag(nltk.word_tokenize(sent))]
+
+    df = pandas.DataFrame(data)
+    df.to_csv(fileName, encoding='utf-8')
 
 def main():
     inputSubdir = 'InputData'
@@ -9,21 +21,21 @@ def main():
     if not os.path.exists(outputSubdir):
         os.makedirs(outputSubdir)
 
-
     posts = pandas.read_csv(os.path.join(inputSubdir, 'posts.csv'), encoding='utf-8')['Text']
-    
+
     sentences = []
     for post in posts:
-        sentences = sentences + nltk.sent_tokenize(post)
-    
-    data = []
-    for sent in random.sample(sentences, 10):
-        data = data + nltk.pos_tag(nltk.word_tokenize(sent))
+        sentences += nltk.sent_tokenize(post)
 
-    df = pandas.DataFrame.from_dict(data)
-    df.columns = ['word', 'tag']
-    df.to_csv(os.path.join(outputSubdir, 'taggedWords.csv'), index=False, encoding='utf-8')
+    qualitySentences = []
+    for sentence in sentences:
+        words = nltk.word_tokenize(sentence)
+        alphabeticalWords = filter(re.compile(r'[a-zA-Z,.?!]+').search, words)
+        if len(words) == len(alphabeticalWords) and len(words) > 10: 
+            qualitySentences.append(sentence)
 
+    tagSentencesFrom(sentences, os.path.join(outputSubdir, 'taggedSentences.csv'))
+    tagSentencesFrom(qualitySentences, os.path.join(outputSubdir, 'taggedQualitySentences.csv'))
     print "Finished"
 
 main()
