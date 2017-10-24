0ec2442dfbcfda830f0bd32ca6717d2ba9a139fe
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-10-24 13:47:37 +0800

    Improvements in post selection

--- 2-DatasetAnalysisAndAnnotation/3-TokenDefinitionAndAnnotation/postSelector.py
+++ 2-DatasetAnalysisAndAnnotation/3-TokenDefinitionAndAnnotation/postSelector.py
@@ -13,7 +13,18 @@ def main():
     selectedPosts = random.sample(posts, 100)
 
     df = pandas.DataFrame(selectedPosts)
+    df.columns = ['post']
     df.to_csv(os.path.join(outputSubdir, 'selectedPosts.csv'), encoding='utf-8')
+
+    selectedPostsSubdir = outputSubdir + '/PostCollection'
+    if not os.path.exists(selectedPostsSubdir):
+        os.makedirs(selectedPostsSubdir)
+
+    for i, post in enumerate(selectedPosts):
+        filename = 'Post' + str(i + 1) + '.txt'
+        with open(os.path.join(selectedPostsSubdir, filename),'w') as f:
+            print >>f, post.encode('utf8')
+            f.close()
 
     print "Finished"
 
