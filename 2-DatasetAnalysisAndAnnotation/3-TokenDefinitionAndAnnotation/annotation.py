af3950669cea61742f631ae95e106604ee159ba5
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-10-29 21:59:32 +0800

    sliced selected posts, so we can annotate them

--- 2-DatasetAnalysisAndAnnotation/3-TokenDefinitionAndAnnotation/annotation.py
+++ 2-DatasetAnalysisAndAnnotation/3-TokenDefinitionAndAnnotation/annotation.py
@@ -24,6 +24,11 @@ def main():
     print("Now writing to csv...")
     outputSubdir = 'OutputData'
     outputDF.to_csv(os.path.join(outputSubdir, 'annotatedPosts.csv'), encoding='utf-8')
+
+    outputDF[:25].to_csv(os.path.join(outputSubdir, 'annotatedPosts1.csv'), encoding='utf-8')
+    outputDF[25:50].to_csv(os.path.join(outputSubdir, 'annotatedPosts2.csv'), encoding='utf-8')
+    outputDF[50:75].to_csv(os.path.join(outputSubdir, 'annotatedPosts3.csv'), encoding='utf-8')
+    outputDF[75:100].to_csv(os.path.join(outputSubdir, 'annotatedPosts4.csv'), encoding='utf-8')
     print("Finished")
     
 
