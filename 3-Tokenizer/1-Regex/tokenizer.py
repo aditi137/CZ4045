2777610554b6f39ca8252c8e66e5ee4235b12b7d
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-10-30 19:32:02 +0800

    Added input data Working in the tokenizer

--- 3-Tokenizer/1-Regex/tokenizer.py
+++ 3-Tokenizer/1-Regex/tokenizer.py
@@ -3,9 +3,34 @@ import pandas
 import os
 from nltk.tokenize import RegexpTokenizer
 
+inputSubdir = 'InputData'
+outputSubdir = 'OutputData'
+
+def cleanRead(filename):
+    df = pandas.read_csv(os.path.join(inputSubdir, filename), encoding='utf-8', names=list(range(1000)))
+    df = df.iloc[1:] #Remove first row, with the column index
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+def createmanuallyAnnotatedPostsForDiff(tokenizer):
+    manuallyAnnotatedPostsSubdir = outputSubdir + '/ManuallyAnnotatedPosts'
+    if not os.path.exists(manuallyAnnotatedPostsSubdir):
+        os.makedirs(manuallyAnnotatedPostsSubdir)
+
+    posts = pandas.DataFrame()
+    posts = posts.append(cleanRead('annotatedPosts1.csv'), ignore_index=True)
+    posts = posts.append(cleanRead('annotatedPosts2.csv'), ignore_index=True)
+    posts = posts.append(cleanRead('annotatedPosts3.csv'), ignore_index=True)
+    #posts = posts.append(cleanRead('annotatedPosts4.csv'))
+
+    posts.drop(posts.columns[0], axis=1, inplace=True)
+        
+    for i, post in posts.iterrows():    
+        filename = 'Post' + str(i + 1) + '.csv'
+        cleanRow = post.to_frame().transpose().dropna(axis=1)
+        cleanRow.to_csv(os.path.join(manuallyAnnotatedPostsSubdir, filename), encoding='utf-8', index=False, header=None)
+
 def main():
-    inputSubdir = 'InputData'
-    outputSubdir = 'OutputData'
     if not os.path.exists(outputSubdir):
         os.makedirs(outputSubdir)
 
@@ -46,13 +71,15 @@ def main():
     postsSubdir = outputSubdir + '/PostCollection'
     if not os.path.exists(postsSubdir):
         os.makedirs(postsSubdir)
-
+        
     for i, post in enumerate(posts):
         words = tokenizer.tokenize(post)
     
         filename = 'Post' + str(i + 1) + '.csv'
         df = pandas.DataFrame(words)
         df.to_csv(os.path.join(postsSubdir, filename), encoding='utf-8')
+
+    createmanuallyAnnotatedPostsForDiff(tokenizer)
 
     print "Finished"
 
